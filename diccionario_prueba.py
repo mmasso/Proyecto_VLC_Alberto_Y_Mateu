9889e29f28c942ae901a054213b1f07c0ec9be6f
Albertomanas <amanas@cifpfbmoll.eu>
2019-11-18 16:44:16 +0100

    build(datos): añadimos diccionario puentear xml.

---
+++ diccionario_prueba.py
@@ -0,0 +1,7 @@
+libreria = {"California_Uber_Alles.mp3":
+                {"track-number": 3, "artist": "Dead Kennedys", "album": "Dead Kennedys", "location": "./biblioteca/California_Uber_Alles.mp3"},
+            "Seattle_Party": 
+                {"track-number": 1, "artist": "Chastity Belt", "album": "No regrets", "location": "./biblioteca/Seattle_Party.flac"},
+            "King_Kunta":
+                {"track-number": 3, "artist": "Kendrick Lamar", "album": "To Pimp A Butterfly", "location": "./biblioteca/King_Kunta.mp3"}   
+            }
