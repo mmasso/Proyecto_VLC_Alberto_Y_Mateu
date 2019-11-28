9cce434378bb4de8a6cc3f5bdc801df408d192db
mmasso <mmasso@cifpfbmoll.eu>
2019-11-28 20:25:51 +0100

    feat(logica): codigo main

---
+++ lista_al_vlc.py
@@ -0,0 +1,8 @@
+import os
+
+
+def lista_a_vlc(lista):
+    canciones = " ".join(lista)
+    rutaVLC = 'C:\\Program Files\\VideoLAN\\VLC\\vlc.exe'
+    comando = rutaVLC + " " + canciones
+    return os.popen(comando)
