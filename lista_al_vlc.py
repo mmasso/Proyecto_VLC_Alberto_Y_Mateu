88b6d617c2b3062fceddc60165f936c30dd410e0
Albertomanas <amanas@cifpfbmoll.eu>
2019-11-29 18:32:01 +0100

    perf(asert precondicion lista_al_vlc): precondición isinstanc(lista,list)

--- lista_al_vlc.py
+++ lista_al_vlc.py
@@ -2,7 +2,9 @@ import os
 
 
 def lista_a_vlc(lista):
+    assert isinstance(lista, list)
     canciones = " ".join(lista)
     rutaVLC = '"C:\\Program Files\\VideoLAN\\VLC\\vlc.exe"'
     comando = rutaVLC + " " + canciones
     return os.popen(comando)
+
