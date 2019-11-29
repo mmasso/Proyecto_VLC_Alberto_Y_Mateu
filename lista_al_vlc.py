32fa4499635c777a134b5ddf344911fb0a99a844
mmasso <mmasso@cifpfbmoll.eu>
2019-11-29 19:36:37 +0100

    refactor(Creacion de rutaVLC ): modificacion del fichero compentando rutaVLC abso.

--- lista_al_vlc.py
+++ lista_al_vlc.py
@@ -3,6 +3,7 @@ import os
 
 def lista_a_vlc(lista):
     canciones = " ".join(lista)
-    rutaVLC = '"C:\\Program Files\\VideoLAN\\VLC\\vlc.exe"'
+    # rutaVLC = '"C:\\Program Files\\VideoLAN\\VLC\\vlc.exe"'
+    rutaVLC = '"vlc"'
     comando = rutaVLC + " " + canciones
     return os.popen(comando)
