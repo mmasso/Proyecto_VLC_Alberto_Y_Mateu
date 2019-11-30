e60ae08cee68b941af9f5fe6ad9a87430d07fa25
mmasso <mmasso@cifpfbmoll.eu>
2019-11-30 17:59:23 +0100

    test(Try except cambios): Try except vlc

--- lista_al_vlc.py
+++ lista_al_vlc.py
@@ -5,7 +5,17 @@ def lista_a_vlc(lista):
     assert isinstance(lista, list)
     canciones = " ".join(lista)
     # rutaVLC = '"C:\\Program Files\\VideoLAN\\VLC\\vlc.exe"'
-    rutaVLC = '"vlc"'
+    rutaVLC = '"vlxc"'
+    try:
+        os.popen(rutaVLC)
+    except OSError:
+        print('El sistema no puede encontrar la ruta especificada')
+    else:
+        pass
     comando = rutaVLC + " " + canciones
     return os.popen(comando)
 
+
+if __name__ == "__main__":
+    lista_a_vlc(['/home/mateu/Escritorio/CancionesVLC/Insert_coin.mp3',
+                 '/home/mateu/Escritorio/CancionesVLC/Because.mp3'])
