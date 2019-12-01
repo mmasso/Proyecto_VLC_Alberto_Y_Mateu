ebc8c7c891f46eafd63aac7c4d01343f789b9386
mmasso <mmasso@cifpfbmoll.eu>
2019-12-01 21:18:32 +0100

    refactor: refactorizar funcion

--- src/paths_para_vlc.py
+++ src/paths_para_vlc.py
@@ -1,6 +1,7 @@
 def dic_lista_paths(diccionario):
     assert isinstance(diccionario, dict)
     listaVLC = []
+    '''
     contador = 1
     while contador < len(diccionario):
         for currentSong in diccionario:
@@ -8,7 +9,15 @@ def dic_lista_paths(diccionario):
                 contador = contador + 1
                 listaVLC.append(diccionario[currentSong]['localizacion'])
     assert listaVLC != []
-    assert len(listaVLC) == len(diccionario)
     return listaVLC
-
+    '''
+    for currentSong in diccionario:
+        listaVLC.append([diccionario[currentSong]['order'], diccionario[currentSong]['localizacion']])
+    listaPath = []
+    listaVLC.sort()
+    for valor in listaVLC:
+        listaPath.append(valor[1])
+    assert len(listaPath) == len(listaVLC) == len(diccionario)
+    assert listaPath != []
+    return listaPath
     # crear lista de tuplas orden localizacion, hacer sort i scar lista de localizacion.
