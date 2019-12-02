ef3596dc3489a35ece16a28e33746bba38a41e73
mmasso <mmasso@cifpfbmoll.eu>
2019-12-02 14:05:07 +0100

    test: test

--- src/random_assign.py
+++ src/random_assign.py
@@ -21,14 +21,9 @@ def random_assign(diccionario):
     assert diccionario_randomizado != {}
     assert len(diccionario_randomizado) == longitudOriginal
     return diccionario_randomizado
-    
+
+
 if __name__ == "__main__":
-    assert isinstance(random_assign({"When It's All Gone": {'album': "When It's All Gone", 'artista': 'Terror Reid',
-                           'compositor': 'Getter', 'genero': 'Hip hop',
-                           'localizacion': '"C:\\Users\\mateu\\Desktop\\CancionesVLC\\When_it_is_all_gone.mp3"'},
-                           'Buried Alive': {'album': 'Buried Alive', 'artista': 'Terror Reid',
-                           'compositor': 'Getter', 'genero': 'Hip hop',
-                           'localizacion': '"C:\\Users\\mateu\\Desktop\\CancionesVLC\\Buried_alive.mp3"'}}), dict)
     assert len(random_assign({"When It's All Gone": {'album': "When It's All Gone", 'artista': 'Terror Reid',
                            'compositor': 'Getter', 'genero': 'Hip hop',
                            'localizacion': '"C:\\Users\\mateu\\Desktop\\CancionesVLC\\When_it_is_all_gone.mp3"'},
