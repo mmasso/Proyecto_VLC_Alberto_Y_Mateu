1d4cd201b0654d6fba05730bfe6b82b84ad072f8
mmasso <mmasso@cifpfbmoll.eu>
2019-12-02 13:59:09 +0100

    style(pycodestyle): pycodestyle

--- src/paths_para_vlc.py
+++ src/paths_para_vlc.py
@@ -2,7 +2,8 @@ def dic_lista_paths(diccionario):
     assert isinstance(diccionario, dict)
     lista_vlc = []
     for currentSong in diccionario:
-        lista_vlc.append([diccionario[currentSong]['order'], diccionario[currentSong]['localizacion']])
+        lista_vlc.append([diccionario[currentSong]['order'],
+                         diccionario[currentSong]['localizacion']])
     lista_path = []
     lista_vlc.sort()
     for valor in lista_vlc:
@@ -10,4 +11,3 @@ def dic_lista_paths(diccionario):
     assert len(lista_path) == len(lista_vlc) == len(diccionario)
     assert lista_path != []
     return lista_path
-
